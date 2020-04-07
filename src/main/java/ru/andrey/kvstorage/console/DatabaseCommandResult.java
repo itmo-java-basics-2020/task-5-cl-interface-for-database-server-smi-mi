13a1733b1399466ecccd348e89c1ca3832aec3b9
Mariia Smirnova <mariia.smirnova@sim-lab.ru>
2020-04-07 21:59:23 +0300

    Implemented DatabaseCommandResult.

--- src/main/java/ru/andrey/kvstorage/console/DatabaseCommandResult.java
+++ src/main/java/ru/andrey/kvstorage/console/DatabaseCommandResult.java
@@ -4,6 +4,14 @@ import java.util.Optional;
 
 public interface DatabaseCommandResult {
 
+    static DatabaseCommandResult success(String result) {
+        return new DatabaseCommandResultDefault(DatabaseCommandStatus.SUCCESS, result, null);
+    }
+
+    static DatabaseCommandResult error(String message) {
+        return new DatabaseCommandResultDefault(DatabaseCommandStatus.FAILED, null, message);
+    }
+
     Optional<String> getResult();
 
     DatabaseCommandStatus getStatus();
@@ -14,5 +22,38 @@ public interface DatabaseCommandResult {
 
     enum DatabaseCommandStatus {
         SUCCESS, FAILED
+    }
+
+    class DatabaseCommandResultDefault implements DatabaseCommandResult {
+
+        @Override
+        public Optional<String> getResult() {
+            return Optional.ofNullable(this.result);
+        }
+
+        @Override
+        public DatabaseCommandStatus getStatus() {
+            return this.status;
+        }
+
+        @Override
+        public boolean isSuccess(){
+            return this.status == DatabaseCommandStatus.SUCCESS;
+        }
+
+        @Override
+        public String getErrorMessage(){
+            return this.message;
+        }
+
+        private DatabaseCommandResultDefault(DatabaseCommandStatus status, String result, String message) {
+            this.status = status;
+            this.result = result;
+            this.message = message;
+        }
+
+        private DatabaseCommandStatus status;
+        private String result;
+        private String message;
     }
 }
